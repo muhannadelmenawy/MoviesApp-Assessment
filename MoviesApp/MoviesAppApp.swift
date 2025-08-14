656d417540d53b3e282109a32ad5c45914851edc
Muhannad El Menawy <voltyks.inc@gmail.com>
2025-08-14 19:39:57 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Muhannad El Menawy on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
